b297e41d76671853df98702f40aa63c53c1824b7
Alejandro-Suarez-Rincon <alejandrosuarez2032002@gmail.com>
2024-08-01 22:41:21 -0500

    feat: add socket client

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,7 +1,47 @@
+"use client";
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+const socket = io(process.env.SOCKET_CLIENT ?? "");
+
 export default function Home() {
+  const [message, setMessage] = useState<string>("");
+  const [messages, setMessages] = useState<string[]>([]);
+
+  useEffect(() => {
+    socket.on("chat message", (msg: string) => {
+      setMessages((prevMessages) => [...prevMessages, msg]);
+    });
+
+    return () => {
+      socket.off("chat message");
+    };
+  }, []);
+
+  const sendMessage = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (message) {
+      socket.emit("chat message", message);
+      setMessage("");
+    }
+  };
+
   return (
     <section>
-      <></>
+      <ul id="messages">
+        {messages.map((msg, index) => (
+          <li key={index}>{msg}</li>
+        ))}
+      </ul>
+      <form onSubmit={sendMessage}>
+        <input
+          id="input"
+          autoComplete="off"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button>Send</button>
+      </form>
     </section>
   );
 }
